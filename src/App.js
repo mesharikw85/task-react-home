e1b9fc3cdca99947e946fee7403543ad23b327d3
mesharikw85 <mkwg85@gmail.com>
2023-05-28 20:16:46 +0300

    task 2 ptodect list

--- src/App.js
+++ src/App.js
@@ -1,14 +1,31 @@
-import { getByTitle } from "@testing-library/react";
 import "./App.css";
-// import imge from "./assest/bookstore.jpg";
-import img from "./assest/bookestore.jpeg";
+
+// import img from "./assest/bookestore.jpeg";
+import birdstore from "./birdstore";
+import birdstore1 from "./assest/birdimg/bird store.png";
 
 function App() {
+  // return (
+  //   <div className="App">
+  //     <h1>bookstore</h1>
+  //     <p>here an old bookstore</p>
+  //     <img src={img} alt="book-store" />
+  //   </div>
+  // );
   return (
     <div className="App">
-      <h1>bookstore</h1>
-      <p>here an old bookstore</p>
-      <img src={img} alt="book-store" />
+      <h1>bird-store1</h1>
+      <p>welcome to lovly bird</p>
+      <img src={birdstore1} alt="picstore" />
+      <h1>{birdstore[0].name}</h1>
+      <h1>{birdstore[0].price}</h1>
+      <img src={birdstore[0].pic} alt="bird" />
+
+      <div>
+        <h1>{birdstore[1].name}</h1>
+        <h1>{birdstore[1].price}</h1>
+        <img src={birdstore[1].image} />
+      </div>
     </div>
   );
 }
---
+++ src/birdstore.js
@@ -0,0 +1,17 @@
+import lovebird from "./assest/birdimg/lovevird.jpeg";
+import bluebird from "./assest/birdimg/bluebird.jpeg";
+
+const birdstore = [
+  {
+    name: "love bird",
+    price: "30kd",
+    pic: lovebird,
+  },
+  {
+    name: "bluebird",
+    price: "40kd",
+    image: bluebird,
+  },
+];
+
+export default birdstore;
